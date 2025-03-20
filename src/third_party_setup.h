3d005cda4cf618b01dd2151fa288de432bd1e056
Tim Lobner <lobner@synopsys.com>
2025-03-20 11:17:41 +0100

    disabeld clang warnings in VMA

--- src/third_party_setup.h
+++ src/third_party_setup.h
@@ -9,5 +9,16 @@
 #define VK_NO_PROTOTYPES
 #include "volk.h"
 
+// on clang, VMA throws a lot of nullable warnings
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnullability-completeness"
+#endif
+
 #define VMA_IMPLEMENTATION
 #include "vk_mem_alloc.h"
+
+// reenable the nullability warnings
+#ifdef __clang__
+#pragma clang pop
+#endif
